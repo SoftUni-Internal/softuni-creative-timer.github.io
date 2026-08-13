06a92b3033233626f190787ae061abc54f2a7c96
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2026-08-13 16:12:28 +0300

    Remove draftkings, update indeavr logo

--- scripts/partners.js
+++ scripts/partners.js
@@ -2,7 +2,6 @@ export const partners = [
     { src: "./public/partners/encorp.png", name: "Encorp.ai" },
     { src: "./public/partners/coca-cola.png", name: "Coca Cola HBC" },
     { src: "./public/partners/createx.png", name: "CreateX" },
-    { src: "./public/partners/draft-kings.png", name: "Draft Kings" },
     { src: "./public/partners/indeavr.png", name: "Indeavr" },
     { src: "./public/partners/postbank.png", name: "Postbank" },
     { src: "./public/partners/superhosting.png", name: "SuperHosting" }
